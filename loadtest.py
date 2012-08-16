3c05674713334d36d203cd6840b5c4097bcacd3c
Alexis Metaireau <alexis@notmyidea.org>
2012-08-16 10:58:15 +0200

    Add some documentation

--- loadtest.py
+++ loadtest.py
@@ -8,8 +8,6 @@ from funkload.FunkLoadTestCase import FunkLoadTestCase
 class MarketplaceTest(FunkLoadTestCase):
 
     def __init__(self, *args, **kwargs):
-        self.root = self.conf_get('main', 'url')
-
         self.languages = (
             'af', 'ar', 'bg', 'ca', 'cs', 'da', 'de', 'el', 'en-US', 'es',
             'eu', 'fa', 'fi', 'fr', 'ga-IE', 'he', 'hu', 'id', 'it', 'ja',
@@ -17,10 +15,21 @@ class MarketplaceTest(FunkLoadTestCase):
             'sq', 'sv-SE', 'uk', 'vi', 'zh-CN', 'zh-TW',
         )
 
-        self._apps = None
+        # this hardcodes the list of categories so no call to the API will be
+        # made to get it.
         self._categories = None
+        self._apps = None
+
+        super(MarketplaceTest, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        self.root = self.conf_get('main', 'url')
 
     def get(self, url, *args, **kwargs):
+        # when GETing an URL, we don't want to follow the links (img, css etc)
+        # as they could be done on the fly by javascript in an obstrusive way.
+        # we also prepend the domain (self.root) to the get calls in this
+        # method.
         return super(MarketplaceTest, self).get(self.root + url,
                                                 load_auto_links=False,
                                                 *args, **kwargs)
@@ -36,7 +45,7 @@ class MarketplaceTest(FunkLoadTestCase):
     @property
     def apps(self):
         """Gets the list of projects currently contained in the marketplace
-        instance
+        instance, and cache it.
         """
         if not self._apps:
             resp = self.get('/en-US/api/apps/search/')
@@ -46,7 +55,7 @@ class MarketplaceTest(FunkLoadTestCase):
 
     @property
     def categories(self):
-        """Gets all the categories from the marketplace"""
+        """Gets all the categories from marketplace, and cache them"""
         if not self._categories:
             resp = self.get('/en-US/api/apps/category/')
             cats = json.loads(resp.body)['objects']
@@ -56,17 +65,10 @@ class MarketplaceTest(FunkLoadTestCase):
     def test_index(self):
         self.get_all('/')
 
-    def test_everything(self):
-        self.test_index()
-        self.test_search()
-        self.test_category()
-        self.test_app_detail()
-
     def test_search(self):
         # make a request that returning all the apps registered in the
         # marketplace.
         self.get_all('/search/?q=')
-
         self.get_all('/search/?q=%s' % self.apps[0])
 
     def test_app_detail(self):
@@ -75,8 +77,15 @@ class MarketplaceTest(FunkLoadTestCase):
     def test_category(self):
         self.get_all('/apps/{category}'.format(category=self.categories[0]))
 
+    def test_everything(self):
+        self.test_index()
+        self.test_search()
+        self.test_category()
+        self.test_app_detail()
+
 
 def slugify(value):
+    """This is the slugify from django, minus an hardcoded workaround"""
     value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
     value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
     slugified = re.sub('[-\s]+', '-', value)
