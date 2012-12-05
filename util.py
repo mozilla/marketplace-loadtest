5d8a7e3c0711abe7d405a8c0125455be75cc32b0
Hanno Schlichting <hanno@hannosch.eu>
2012-12-05 17:10:29 +0100

    provide a default password, as set in the zamboni test vaurien settings

--- util.py
+++ util.py
@@ -1,9 +1,13 @@
 import os
+import os.path
 
 
 DEFAULT_LOCATION = os.path.join('/var', 'loadtest', 'loadtest')
 
 
 def read_password(location=DEFAULT_LOCATION):
-    with open(location) as f:
-        return f.read().strip()
+    if os.path.exists(location):
+        with open(location) as f:
+            return f.read().strip()
+    # hard-coded token from zamboni settings_test_vaurien
+    return 'sqldkjqlskjd34'
